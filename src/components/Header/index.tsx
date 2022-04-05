330251658297970e37ab7ebe8fb43b8a33b1ba47
Leonardo Martinelli Deves <leo_lli@hotmail.com>
2022-04-05 20:14:46 -0300

    Add files via upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { SignInButton } from "../SignInButton";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
+
+export function Header() {
+  const { asPath } = useRouter();
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
